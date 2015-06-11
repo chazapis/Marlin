8955ea68188f011847667cf8e9e8ae2b9ccbccfc
Antony Chazapis <chazapis@gmail.com>
2015-06-11 08:42:53 +0300

    Fine tune extruder

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -490,7 +490,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 
 // default settings
 
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {100.50,100.50,4000,650}  // default steps per unit for Ultimaker
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {100.50,100.50,4000,661}  // default steps per unit for Ultimaker
 #define DEFAULT_MAX_FEEDRATE          {500, 500, 2, 25}    // (mm/sec)
 #define DEFAULT_MAX_ACCELERATION      {9000,9000,100,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for Skeinforge 40+, for older versions raise them a lot.
 
